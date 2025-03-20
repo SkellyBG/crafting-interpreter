f3167a174fcdcf1e84eb2ab9b909b7434164cff5
Hung <skellybg245@gmail.com>
2025-03-21 00:17:28 +1100

    bit more

--- src/expr.rs
+++ src/expr.rs
@@ -1,17 +1,35 @@
 use crate::token::Token;
 
+enum BinOp {
+    EqualEqual,
+    BangEqual,
+    Less,
+    LessEqual,
+    Greater,
+    GreaterEqual,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+}
+
+enum UnOp {
+    Minus,
+    Bang,
+}
+
 enum Expr {
     Literal,
     Grouping {
         expression: Box<Expr>,
     },
     Unary {
-        operator: Box<Token>,
+        operator: UnOp,
         right: Box<Expr>,
     },
     Binary {
         left: Box<Expr>,
-        operator: Box<Token>,
+        operator: BinOp,
         right: Box<Expr>,
     },
     Operator,
